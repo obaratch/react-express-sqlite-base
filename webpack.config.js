a7f34c69126f7636e6b244069202ba15f52c6e88
obaratch <takashi.obara@obalab.com>
2021-05-09 04:36:09 +0900

    node 16

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,31 @@
 const webpack = require("webpack");
 const nodepath = require("path");
-const resolvePath = (path) => nodepath.resolve(__dirname, path);
+const resolvePath = (path) => nodepath.resolve(__dirname, "./app-client", path);
 const { NODE_ENV = "" } = process.env;
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
-const BASE_PATH = "./app-client";
+const isDev = NODE_ENV.toLowerCase().startsWith("dev");
 
 module.exports = {
-  mode: NODE_ENV.toLowerCase().startsWith("dev") ? "development" : "production",
+  mode: isDev ? "development" : "production",
+  devtool: isDev ? "eval-source-map" : "source-map",
+  devServer: {
+    historyApiFallback: true,
+  },
 
   watchOptions: {
     ignored: ["node_modules", "app-server", "stories", "test", "dist"],
   },
 
-  context: resolvePath(`${BASE_PATH}/src`),
+  context: resolvePath("./src"),
   entry: ["./js/main.js"],
 
   output: {
     publicPath: "/",
-    path: resolvePath(`${BASE_PATH}/dist`),
+    path: resolvePath("./dist"),
     filename: "bundle.js",
   },
 
@@ -53,6 +58,14 @@ module.exports = {
     new webpack.DefinePlugin({
       __$APP_VER$__: `"${require("./package.json").version}"`,
       __$BUILD_TIME$__: Date.now(),
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: resolvePath("./src/assets"),
+          to: resolvePath("./dist/"),
+        },
+      ],
     }),
   ],
 };
